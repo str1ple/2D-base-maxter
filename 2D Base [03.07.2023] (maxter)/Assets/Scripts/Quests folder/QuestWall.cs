26dc9bb693791ae2b32ff510b2b280a54dfdad0d
str1ple <i.wot.love@yandex.ru>
2023-07-09 15:08:25 +0300

    dont fixed diapasone error

--- 2D Base [03.07.2023] (maxter)/Assets/Scripts/Quests folder/QuestWall.cs
+++ 2D Base [03.07.2023] (maxter)/Assets/Scripts/Quests folder/QuestWall.cs
@@ -13,7 +13,7 @@ public class QuestWall : MonoBehaviour
 
     public void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag != "Player" && other.GetComponent<Pickup>().id == 1)
+        if (other.tag != "Player" && other.GetComponent<Pickup>().id == 2)
         {
             Destroy(other.gameObject);
             anim.SetTrigger("isTriggered");
